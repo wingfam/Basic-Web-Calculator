a394806b44d1177ae193dd375146c29daa32fb1e
wingfam <supmangcua970@gmail.com>
2024-03-21 15:37:39 +0700

    - added clearAll(), clearEntry() - fixed some calculation logic

--- scripts.js
+++ scripts.js
@@ -1,62 +1,65 @@
-/* 
-    
-*/
+let firstNum = null;
+let secondNum = null;
+let finalResult = null;
+let firstOp = null;
+let secondOp = null;
 
-let firstNum = 0;
-let secondNum = 0;
-let finalResult = 0;
-let firstOp = "";
-let secondOp = "";
-
-const outputDiv = document.getElementById("calc-output");
+const entryDiv = document.getElementById("calc-entry");
 const resultDiv = document.getElementById("result-num");
 
-const setEventListener = function() {
-    const buttons = document.querySelectorAll(".btn")
-    
-    for (let button of buttons) {
-        button.addEventListener('click', function(e) {
-            // console.log(this.className.includes("btn-num")); // logs the className of my_element
-            // console.log(e.currentTarget === this); // logs `true
-            // console.log(e.currentTarget.value)
-            if (this.className.includes("btn-num")) {
-                outputDiv.textContent = parseFloat(outputDiv.textContent.concat("", e.currentTarget.value));
-                if (outputDiv.textContent.length > 15) {
-                    outputDiv.textContent = outputDiv.textContent.substring(0, 15)
-                }
-            } else if (this.className.includes("btn-operator")) {
-                let currentOp = e.currentTarget.value
+const setEventListener = function () {
+    const buttons = document.querySelectorAll(".btn");
 
+    for (let button of buttons) {
+        button.addEventListener("click", function (e) {
+            if (this.className.includes("btn-num")) {
+                if (entryDiv.textContent.substring(0, 1).includes("0")) entryDiv.textContent = entryDiv.textContent.replace("0", "");
+
+                entryDiv.textContent = entryDiv.textContent.concat("", e.currentTarget.value);
+
+                if (entryDiv.textContent.length > 10) entryDiv.textContent = entryDiv.textContent.substring(0, 10);
+            } else if (this.className.includes("btn-dot")) {
+                if (entryDiv.textContent.includes(".")) return;
+                else entryDiv.textContent = entryDiv.textContent.concat("", e.currentTarget.value);
+            } else if (this.className.includes("btn-operator")) {
+                let currentOp = e.currentTarget.value;
+
+                // Here I deduced that there are two circumstances to use if else like this:
+                // - First, new calculation from the start. 1st operator and 2nd operator haven't entered yet.
+                // - Second, a calculation has already took place, and every calculation after that.
                 if (!firstOp && !secondOp) {
                     firstOp = currentOp;
-                    firstNum = parseFloat(outputDiv.textContent);
-                    resultDiv.textContent = outputDiv.textContent.concat(" ", currentOp);
+                    firstNum = parseFloat(entryDiv.textContent);
+                    resultDiv.textContent = entryDiv.textContent.concat(" ", currentOp);
                 } else if (firstOp && !secondOp) {
                     secondOp = currentOp;
-                    secondNum = parseFloat(outputDiv.textContent);
+                    secondNum = parseFloat(entryDiv.textContent);
                 }
 
-                clear();
+                entryDiv.textContent = entryDiv.textContent
+                    .replaceAll(/[0-9]/g, "0")
+                    .substring(0, 1);
+
                 operate("btn-operator");
-            } else if (this.className.includes("btn-equal")) {
-                operate("btn-equal");
-            }
+            } else if (this.className.includes("btn-equal")) operate("btn-equal");
+            else if (this.className.includes("btn-all-clear")) clearAll();
+            else if (this.className.includes("btn-clear-entry")) clearEntry(false);
         });
     }
-}
+};
 
-const operate = function(btnType) {
+const operate = function (btnType) {
     if (btnType.includes("equal")) {
-        // if (!firstNum && !firstOp && !secondNum) return
-        if (firstNum && firstOp && !secondNum) secondNum = parseFloat(outputDiv.textContent);
+        if (firstNum && firstOp && !secondNum)
+            secondNum = parseFloat(entryDiv.textContent);
     }
 
-    if (firstNum === 0 && firstOp === "/") {
-        resultDiv.textContent = "Oops, can't divide by 0!"
+    if (secondNum === 0 && firstOp === "/") {
+        resultDiv.textContent = "Oops, can't divide by 0!";
         return;
     }
-    
-    if (firstNum, firstOp, secondNum) {
+
+    if ((firstNum, firstOp, secondNum)) {
         switch (firstOp) {
             case "+":
                 finalResult = firstNum + secondNum;
@@ -75,20 +78,35 @@ const operate = function(btnType) {
         }
 
         resultDiv.textContent = finalResult.toString().concat(" ", secondOp);
+        entryDiv.textContent = entryDiv.textContent
+            .replaceAll(/[0-9]/g, "0")
+            .substring(0, 1);
 
+        // Shift each variable with new value so new calculation can take place.
         firstNum = finalResult;
         secondNum = 0;
         finalResult = 0;
         firstOp = secondOp;
-        secondOp = "";
-
-        clear();
     }
-}
 
-const clear = function() {
-    outputDiv.textContent = outputDiv.textContent.replaceAll(/[0-9]/g, "0")
-    outputDiv.textContent = outputDiv.textContent.substring(0, 1);
-}
+    // Make sure the second operator is empty so I can set secondNum to a new number every time.
+    secondOp = "";
+};
+
+const clearEntry = function (isClearAll) {
+    entryDiv.textContent = entryDiv.textContent
+        .replaceAll(/[0-9]/g, "0")
+        .substring(0, 1);
+    if (isClearAll) resultDiv.textContent = parseInt(resultDiv.textContent) * 0;
+};
+
+const clearAll = function () {
+    firstNum = 0;
+    secondNum = 0;
+    finalResult = 0;
+    firstOp = "";
+    secondOp = "";
+    clearEntry(true);
+};
 
 setEventListener();
